de3090f59dbaf7408dcfc7496418854a09203a2d
Mostafa <mostafaelfallal90@gmail.com>
2025-04-10 18:25:00 +0200

    replacing package version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '372f2c8aea9941e187dd7bde907cf134875c040d'
+AESD_ASSIGNMENTS_VERSION = 'f335e3704199597f593e50e6cdbae36e742d77ce'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
